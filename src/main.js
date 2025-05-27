9bdf910a8198d4cdb3804b96db91e6c37e63383a
Yevhen Bozadzhi <ebozadji13@gmail.com>
2025-05-27 17:39:02 +0300

    last changes

--- src/main.js
+++ src/main.js
@@ -18,69 +18,63 @@ const loadMoreBtn = document.querySelector('.load-more');
 let currentPage = 1;
 let query = '';
 let totalPage = 0;
-let per_page = 15;
-// let isLoading = false;
+const per_page = 15;
 
 async function searchImages(reset = false) {
-  // if (isLoading) return; 
   if (!query) return;
 
-  // isLoading = true;
   try {
     showLoader();
+
+    if (reset) {
+      currentPage = 1; 
+    }
+
     const images = await getImagesByQuery(query, currentPage);
     hideLoader();
-    if (images.total === 0) {
+
+    if (images.total === 0 || images.hits.length === 0) {
       iziToast.warning({
         title: 'Oops!',
         message: 'No images found. Try another word!',
         position: 'topRight',
       });
       clearGallery();
+      hideLoadMore(); 
       return;
     }
 
     if (reset) {
       clearGallery();
     }
+
     createGallery(images.hits);
 
+    totalPage = Math.ceil(images.totalHits / per_page); 
 
-    
-    const totalPage = images.total / per_page;
-    if (totalPage > currentPage) {
+    if (currentPage < totalPage) {
       showLoadMore();
-    }
-    if (totalPage <= currentPage) {
+    } else {
       hideLoadMore();
       iziToast.info({
         title: 'Oops!',
         message: 'We are sorry, but you have reached the end of search results.',
         position: 'topRight',
       });
-      currentPage = 1;
     }
 
- 
-    
+    if (!reset) {
+      const { height } = document
+        .querySelector('.gallery')
+        .lastElementChild.getBoundingClientRect();
 
-
-
-    
-if (!reset) {
-  window.scrollBy({
-    top: 400,  
-    behavior: 'smooth'
-  });
-}
-
-
-    // if (images.total >= 15) {
-    //   loadMoreBtn.style.display = 'block'; 
-    // } else {
-    //   loadMoreBtn.style.display = 'none';
-    // }
+      window.scrollBy({
+        top: height * 2,
+        behavior: 'smooth',
+      });
+    }
   } catch (error) {
+    hideLoader();
     iziToast.error({
       title: 'Error',
       message: `Something went wrong: ${error.message}`,
@@ -101,4 +95,3 @@ loadMoreBtn.addEventListener('click', () => {
   currentPage += 1;
   searchImages(false);
 });
-
